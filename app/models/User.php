074fd520424965b97b7f93b08040f0cfc509adb0
nekwebdev <nek.webdev@server.fake>
2013-06-24 00:14:01 -1000

    install of zizaco/confide zizaco/entrust

--- app/models/User.php
+++ app/models/User.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
+use Zizaco\Confide\ConfideUser;
+use Zizaco\Entrust\HasRole;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends ConfideUser implements UserInterface, RemindableInterface {
+
+	use HasRole;
 
 	/**
 	 * The database table used by the model.
